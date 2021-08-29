6e38ef8938f6161dd0d82db36aa0ee56cf127f77
gurumarg <gurumargdarshan14@gmail.com>
2021-08-29 15:46:09 +0530

    user page functionality

--- Gurumarg2/urls.py
+++ Gurumarg2/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
     path('question_form',include('common.urls')),
-
+    path('users_page',include('admin.urls')),
 
 ]
--- admin/urls.py
+++ admin/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
      path('users_page',views.users_page,name='users_page'),
-
+     path('ss_data',views.ss_data,name='ss_data'),
+     path('add_ss',views.add_ss,name = 'add_ss'),
+     path('edit_ss_data',views.edit_ss_data,name='edit_ss_data'),
+     path('listof_main_admin',views.listof_main_admin,name='listof_main_admin'),
 
 ]
