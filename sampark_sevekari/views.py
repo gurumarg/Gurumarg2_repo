5e8c4e9fd622bbbf1588c0136594f66e49c22f3e
gurumarg <gurumargdarshan14@gmail.com>
2021-09-03 17:49:22 +0530

    admin is complete

--- sampark_sevekari/views.py
+++ sampark_sevekari/views.py
@@ -14,17 +14,23 @@ def listof_SS_prashankarta(request):
     user_details = get_user_model().objects.get(pk=id)
     record = sampark_sevekari.objects.get(User_id_id = id)
     sscode = record.sscode
-    all_prashankarta = get_user_model().objects.filter(sscode=sscode).filter(type='verified')
+    all_prashankarta = get_user_model().objects.filter(sscode=sscode)
     function1 = 'listof_SS_prashankarta'
     if button_clicked == 'prashnakarta':
         x = 'prashnakarta'
-    if button_clicked == 'prashnakarta_seva':
+    elif button_clicked == 'prashnakarta_seva':
         x = 'prashnakarta_seva'
+    elif button_clicked == 'tumche_pk':
+        x = 'tumche_pk'
     else:
         x = ''
-    return render(request,'home_sampark_sevekari.html',{'user_details':user_details,
+    if user_details.type == 'sampark_sevekari':
+         return render(request,'home_sampark_sevekari.html',{'user_details':user_details,
                                 'function1':function1,'data':all_prashankarta,'x':x})
 
+    elif user_details.type == 'main_admin':
+        return render(request, 'users_page.html', {'user_details': user_details,
+                                  'function1': function1, 'data': all_prashankarta, 'x': x})
 
 def listof_SS_rejected(request):
     id = request.POST['user']
@@ -42,8 +48,10 @@ def select_pp_question_seva(request):
     user_details = get_user_model().objects.get(pk=id)
     pk_table = session_data.objects.filter(User_id_id = pk_id)
     function2 = 'display questions'
-    return render(request,'home_sampark_sevekari.html',{'user_details':user_details,'pk_table':pk_table, 'function2':function2})
-
+    if user_details.type == 'sampark_sevekari':
+            return render(request,'home_sampark_sevekari.html',{'user_details':user_details,'pk_table':pk_table, 'function2':function2})
+    elif user_details.type == 'main_admin':
+            return render(request,'users_page.html',{'user_details':user_details,'pk_table':pk_table, 'function2':function2})
 
 def view_seva_ss(request):
     session_id = request.POST['select_session_id']
@@ -65,10 +73,12 @@ def view_seva_ss(request):
         print('SS sevadic', sevadic)
     else:
         function2 = 'multiple_seva_id'
+    if user_details.type == 'samapark_sevekari':
 
-    return render(request, 'home_sampark_sevekari.html',{'user_details': user_details,'dic1':dic1,'sd':sevadic,'function2': function2,'session_id':session_id})
+         return render(request, 'home_sampark_sevekari.html',{'user_details': user_details,'dic1':dic1,'sd':sevadic,'function2': function2,'session_id':session_id})
 
-
+    elif user_details.type == 'main_admin':
+        return render(request, 'users_page.html',{'user_details': user_details, 'dic1': dic1, 'sd': sevadic, 'function2': function2,'session_id': session_id})
 
 
 def view_SS_selectedsevaid(request):
@@ -81,8 +91,13 @@ def view_SS_selectedsevaid(request):
     sevadic = make_seva_dic(sevadetails)
     print('sevadic return value',sevadic)
     function2 = 'user_seva_display'
-    return render(request, 'home_sampark_sevekari.html',
+    if user_details.type == 'sampark_sevekari':
+         return render(request, 'home_sampark_sevekari.html',
                   {'user_details': user_details, 'dic1': dic1, 'sd': sevadic, 'function2': function2})
+    elif user_details.type == 'main_admin':
+        return render(request, 'users_page.html',
+                      {'user_details': user_details, 'dic1': dic1, 'sd': sevadic, 'function2': function2})
+
 
 def update_seva_ss(request):
     seva_expalined = request.POST['seva_expalined']
@@ -97,7 +112,7 @@ def update_seva_ss(request):
     if type == 'sampark_sevekari':
         return render(request, 'home_sampark_sevekari.html', {'user_details': user_details})
     elif type == 'main_admin':
-        return render(request, 'home_admin.html', {'user_details': user_details})
+        return render(request, 'users_page.html', {'user_details': user_details})
 
 
 
